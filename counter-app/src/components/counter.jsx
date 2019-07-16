84dd051e98ee2e3ce4300139a3360dc09a8bdc23
Blair Cox <wblaircox@gmail.com>
2019-07-16 15:50:15 -0300

    4 - Passing Children

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -19,9 +19,11 @@ class Counter extends Component {
 
   render() {
     //console.log("props", this.props);
+    console.log(this.props);
 
     return (
       <div>
+        <h4>{this.props.id}</h4>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.handleIncrement()} //this does not work, product not defined.
--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -15,7 +15,9 @@ class Counters extends Component {
     return (
       <div>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value} />
+          <Counter key={counter.id} value={counter.value} id={counter.id}>
+            <h4>Counter #{counter.id}</h4>
+          </Counter>
         ))}
       </div>
     );
