207b6a476dd1d0519ab32365cc6c5c85178f0d54
Blair Cox <wblaircox@gmail.com>
2019-07-16 09:42:04 -0300

    Lec 9 - Conditional Rendering

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -11,29 +11,25 @@ class Counter extends Component {
     fontWeight: "bold"
   };
 
-  render() {
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
     return (
-      <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </div>
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
 
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+  render() {
+    return (
+      <div>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
+      </div>
+    );
   }
 }
 
