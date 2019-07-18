fbaef98c7d7a670b930c39c74885bc61c740f9f2
Blair Cox <wblaircox@gmail.com>
2019-07-18 19:39:36 -0300

    14 - Destructing Arguments

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 
 // Statless Functional Component
-const NavBar = props => {
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Navbar{" "}
         <span className="badge badge-pill badge-secondary">
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
